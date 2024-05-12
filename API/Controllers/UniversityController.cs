ff8fbff9bd4484b21e901635704189d3a4cf7236
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-05-12 13:32:08 +0200

    create controller

---
+++ API/Controllers/UniversityController.cs
@@ -0,0 +1,28 @@
+﻿using Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UniversityController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UniversityController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("getUniversities")]
+
+        public IActionResult GetUniversities()
+        {
+            var uni = _context.Universities.Select(x => new { x.Id, x.CountryId });
+
+            return Ok(uni);
+        }
+    }
+}
