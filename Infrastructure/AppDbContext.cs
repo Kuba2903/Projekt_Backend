9d111a4927c39f769fcc09fa0f53a84efcaff5ed
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-05-12 13:19:41 +0200

    create entities

---
+++ Infrastructure/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;user=root;password=pass;database=universities");
+
+
+        }
+    }
+}
